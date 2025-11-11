9c9d48632cdc98b8a08fd965c0f7a7a7da395837
DINESH K <dinesh0392001@gmail.com>
2025-11-11 05:58:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4707735030-b6bbc",
+  "appId": "1:1083832736738:web:62c90a932b1eb5b99bb7bb",
+  "apiKey": "AIzaSyDoBvAmbGL1PRln0Ik088_dPStUdxvO7ps",
+  "authDomain": "studio-4707735030-b6bbc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1083832736738"
+};
