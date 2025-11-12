7a817991a7073760591d973ed7aaa042d773c8fe
DINESH K <dinesh0392001@gmail.com>
2025-11-12 12:58:07 +0000

    specific mail only

--- src/lib/allowed-emails.ts
+++ src/lib/allowed-emails.ts
@@ -3,10 +3,14 @@
 // Note: Emails are case-insensitive.
 
 export const allowedEmails: string[] = [
-  'admin@example.com',
-  'user1@example.com',
-  'user2@example.com',
-  'dinesh0392001@gmail.com', // Example user
+  'dhanabalmorshal@gmail.com',
+  'kumaresanvijay2002@gmail.com',
+  'sridharan01234@gmail.com',
+  'dinesh0392001@gmail.com',
+  'gowthamankittusamy@gmail.com',
+  'gopinathramesh65@gmail.com',
+  'tkdharanesh@gmail.com',
+  'balajivenkat1302@gmail.com',
 ];
 
 // Function to check if an email is in the whitelist (case-insensitive)
